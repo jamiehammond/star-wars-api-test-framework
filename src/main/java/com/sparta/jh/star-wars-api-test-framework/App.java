10961e240a077b4534fedf190cb7f0e5fa034b67
Jamie Hammond <33955664+jamiehammond@users.noreply.github.com>
2021-01-13 12:32:10 +0000

    Initial commit

---
+++ src/main/java/com/sparta/jh/star-wars-api-test-framework/App.java
@@ -0,0 +1,13 @@
+package com.sparta.jh.star-wars-api-test-framework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
