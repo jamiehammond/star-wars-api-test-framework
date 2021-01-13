ea991a1eaab442f2be6e3ef86426e2b8ca533be5
Jamie Hammond <33955664+jamiehammond@users.noreply.github.com>
2021-01-13 15:05:19 +0000

    Added method body and test for getStatusCode.

--- src/test/java/frameworktest/ConnectionControllerTest.java
+++ src/test/java/frameworktest/ConnectionControllerTest.java
@@ -20,4 +20,15 @@ class ConnectionControllerTest {
     void getUrlReturnsCorrectUrl() {
         fail("Not implemented");
     }
+
+    @Test
+    @DisplayName("getStatusCode returns valid http status code")
+    void getStatusCodeReturnsValidHttpStatusCode() {
+        int statusCode = ConnectionController.getStatusCode();
+        Assertions.assertTrue((statusCode >= 100 && statusCode <= 103) ||
+                (statusCode >= 200 && statusCode <= 226) ||
+                (statusCode >= 300 && statusCode <= 308) ||
+                (statusCode >= 400 && statusCode <= 451) ||
+                (statusCode >= 500 && statusCode <= 511));
+    }
 }
