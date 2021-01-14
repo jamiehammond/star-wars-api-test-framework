6a768b3f63bbcd1160fea0a4eb7e0314afaee564
Jamie Hammond <33955664+jamiehammond@users.noreply.github.com>
2021-01-14 14:09:54 +0000

    Refactored APIConnectionControllerTest.

--- src/test/java/frameworktest/APIConnectionControllerTest.java
+++ src/test/java/frameworktest/APIConnectionControllerTest.java
@@ -4,6 +4,7 @@ import com.sparta.jh.starwarsapi.framework.connection.APIConnection;
 import com.sparta.jh.starwarsapi.framework.connection.APIConnectionController;
 import com.sparta.jh.starwarsapi.framework.dtos.Resources;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,25 +12,33 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 class APIConnectionControllerTest {
 
+    private static APIConnection person1Connection;
+    private static APIConnection person100Connection;
+    private static APIConnection person001Connection;
+
+    @BeforeAll
+    static void setup() {
+        person1Connection = APIConnectionController.getConnection(Resources.PERSON, 1);
+        person100Connection = APIConnectionController.getConnection(Resources.PERSON, 100);
+        person001Connection = APIConnectionController.getConnection(Resources.PERSON, 001);
+    }
+
     @Test
     @DisplayName("getConnection returns a connection")
     void getConnectionReturnsAConnection() {
-        APIConnection person1Connection = APIConnectionController.getConnection(Resources.PERSON, 1);
         Assertions.assertNotNull(person1Connection);
     }
 
     @Test
     @DisplayName("getConnection returns a connection when ID has leading zeroes")
     void getConnectionReturnsAConnectionWhenIDHasLeadingZeroes() {
-        APIConnection person1Connection = APIConnectionController.getConnection(Resources.PERSON, 00001);
-        Assertions.assertNotNull(person1Connection);
+        Assertions.assertNotNull(person001Connection);
     }
 
     @Test
     @DisplayName("getConnection returns null when invalid resourceID provided")
     void getConnectionReturnsNullWhenInvalidResourceIdProvided() {
-        APIConnection connection = APIConnectionController.getConnection(Resources.PERSON, 100);
-        Assertions.assertNull(connection);
+        Assertions.assertNull(person100Connection);
     }
 
     @Test
